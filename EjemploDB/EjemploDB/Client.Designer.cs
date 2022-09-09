10da32796e2df2a8fecf665d9f54b9f1fbcf4dc6
IvyMasterDiego <diegolobato82@gmail.com>
2022-09-08 20:52:16 -0400

    Vista de cliente

---
+++ EjemploDB/EjemploDB/Client.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace EjemploDB
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lbldesc = new System.Windows.Forms.Label();
+            this.txtdesc = new System.Windows.Forms.TextBox();
+            this.lbldate = new System.Windows.Forms.Label();
+            this.txtdate = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label16 = new System.Windows.Forms.Label();
+            this.cblRestrictions = new System.Windows.Forms.CheckedListBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.cblPermissions = new System.Windows.Forms.CheckedListBox();
+            this.cbLicenseType = new System.Windows.Forms.ComboBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(41, 30);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 0;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(38, 14);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(35, 13);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "Name";
+            // 
+            // lbldesc
+            // 
+            this.lbldesc.AutoSize = true;
+            this.lbldesc.Location = new System.Drawing.Point(38, 65);
+            this.lbldesc.Name = "lbldesc";
+            this.lbldesc.Size = new System.Drawing.Size(60, 13);
+            this.lbldesc.TabIndex = 3;
+            this.lbldesc.Text = "Description";
+            // 
+            // txtdesc
+            // 
+            this.txtdesc.Location = new System.Drawing.Point(41, 81);
+            this.txtdesc.Name = "txtdesc";
+            this.txtdesc.Size = new System.Drawing.Size(100, 20);
+            this.txtdesc.TabIndex = 2;
+            // 
+            // lbldate
+            // 
+            this.lbldate.AutoSize = true;
+            this.lbldate.Location = new System.Drawing.Point(38, 111);
+            this.lbldate.Name = "lbldate";
+            this.lbldate.Size = new System.Drawing.Size(37, 13);
+            this.lbldate.TabIndex = 5;
+            this.lbldate.Text = "Fecha";
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(41, 127);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(100, 20);
+            this.txtdate.TabIndex = 4;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(29, 662);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 6;
+            this.btnadd.Text = "add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(110, 662);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 7;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(191, 662);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 8;
+            this.btncancel.Text = "cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(360, 662);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(461, 662);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 10;
+            this.btndelete.Text = "delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 449);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(792, 207);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(691, 222);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(22, 22);
+            this.button6.TabIndex = 84;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(403, 282);
+            this.label16.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(100, 13);
+            this.label16.TabIndex = 83;
+            this.label16.Text = "Access Restrictions";
+            // 
+            // cblRestrictions
+            // 
+            this.cblRestrictions.FormattingEnabled = true;
+            this.cblRestrictions.Location = new System.Drawing.Point(403, 302);
+            this.cblRestrictions.Margin = new System.Windows.Forms.Padding(2);
+            this.cblRestrictions.Name = "cblRestrictions";
+            this.cblRestrictions.Size = new System.Drawing.Size(284, 124);
+            this.cblRestrictions.TabIndex = 82;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(21, 282);
+            this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(117, 13);
+            this.label15.TabIndex = 81;
+            this.label15.Text = "Application Permissions";
+            // 
+            // cblPermissions
+            // 
+            this.cblPermissions.FormattingEnabled = true;
+            this.cblPermissions.Location = new System.Drawing.Point(21, 302);
+            this.cblPermissions.Margin = new System.Windows.Forms.Padding(2);
+            this.cblPermissions.Name = "cblPermissions";
+            this.cblPermissions.Size = new System.Drawing.Size(284, 124);
+            this.cblPermissions.TabIndex = 80;
+            // 
+            // cbLicenseType
+            // 
+            this.cbLicenseType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLicenseType.FormattingEnabled = true;
+            this.cbLicenseType.Items.AddRange(new object[] {
+            "License #1",
+            "License #2",
+            "Licence #3"});
+            this.cbLicenseType.Location = new System.Drawing.Point(514, 224);
+            this.cbLicenseType.Margin = new System.Windows.Forms.Padding(2);
+            this.cbLicenseType.Name = "cbLicenseType";
+            this.cbLicenseType.Size = new System.Drawing.Size(174, 21);
+            this.cbLicenseType.TabIndex = 79;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(401, 228);
+            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(83, 13);
+            this.label14.TabIndex = 78;
+            this.label14.Text = "License Type";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(132, 247);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(173, 20);
+            this.txtPassword.TabIndex = 77;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(19, 250);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(61, 13);
+            this.label12.TabIndex = 76;
+            this.label12.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Location = new System.Drawing.Point(132, 224);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(173, 20);
+            this.txtUsername.TabIndex = 75;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(19, 227);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(63, 13);
+            this.label13.TabIndex = 74;
+            this.label13.Text = "Username";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(11, 184);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(796, 260);
+            this.tabControl1.TabIndex = 73;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.AliceBlue;
+            this.tabPage1.Controls.Add(this.lblPassword);
+            this.tabPage1.Controls.Add(this.lblUsername);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(788, 234);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login Access Details";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Red;
+            this.lblPassword.Location = new System.Drawing.Point(293, 43);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(21, 13);
+            this.lblPassword.TabIndex = 43;
+            this.lblPassword.Text = "(x)";
+            this.lblPassword.Visible = false;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.Red;
+            this.lblUsername.Location = new System.Drawing.Point(295, 20);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(21, 13);
+            this.lblUsername.TabIndex = 42;
+            this.lblUsername.Text = "(x)";
+            this.lblUsername.Visible = false;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 698);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.cblRestrictions);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.cblPermissions);
+            this.Controls.Add(this.cbLicenseType);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.lbldate);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.lbldesc);
+            this.Controls.Add(this.txtdesc);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.txtname);
+            this.Name = "Client";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Client_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lbldesc;
+        private System.Windows.Forms.TextBox txtdesc;
+        private System.Windows.Forms.Label lbldate;
+        private System.Windows.Forms.TextBox txtdate;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.CheckedListBox cblRestrictions;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.CheckedListBox cblPermissions;
+        private System.Windows.Forms.ComboBox cbLicenseType;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+    }
+}
---
+++ EjemploDB/EjemploDB/Client.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploDB
+{
+    public partial class Client : Form
+    {
+        INTEC_AGU_OCT22Entities db = new INTEC_AGU_OCT22Entities();
+        List<string> msg = new List<string>();
+        string ClientId = string.Empty;
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            GetClient();
+        }
+
+        private void GetClient()
+        {
+            var client = (from a in db.ClientTypes
+                          select new
+                          {
+                              a.Id,
+                              FullName = a.Name,
+                              a.Enabled,
+                              a.CreatedDate,
+                              a.Description
+                          }).ToList();
+            dataGridView1.DataSource = client;
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            btnadd.Enabled = true;
+            btnsave.Enabled = false;
+            btnsave.ForeColor = Color.Green;
+            btncancel.Enabled = true;
+        }
+
+        private void SaveForm()
+        {
+            var cliente = new Person();
+            cliente.ClientType.Id = Convert.ToInt32(Guid.NewGuid().ToString());
+            cliente.ClientType.Name = txtname.Text;
+            cliente.ClientType.Description = txtdesc.Text;
+            cliente.ClientType.CreatedDate = Convert.ToDateTime(txtdate.Text);
+
+            db.ClientTypes.Add(cliente.ClientType);
+            var clientsaved = db.SaveChanges() > 0;
+
+            if (clientsaved)
+            {
+                var user = new User();
+                user.Id = Guid.NewGuid().ToString();
+                user.Username = txtUsername.Text;
+                user.Password = txtPassword.Text;
+                user.LicenseTypeId = Convert.ToInt32(cbLicenseType.SelectedValue);
+                // user.cl = cliente.Id;
+                user.CreatedDate = DateTime.Now;
+
+                db.Users.Add(user);
+                var userSaved = db.SaveChanges() > 0;
+
+                if (userSaved)
+                {
+                    MessageBox.Show("The Client has been added", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    GetClient();
+                    DefaultControls();
+
+                    btnadd.Enabled = true;
+                    btnsave.Enabled = false;
+                    btnsave.ForeColor = Color.Black;
+                    btncancel.Enabled = false;
+                }
+            }
+        }
+
+        private void DefaultControls()
+        {
+            txtdate.Text = string.Empty;
+            txtdesc.Text = string.Empty;
+            txtname.Text = string.Empty;
+        }
+
+        private bool ValidateForm()
+        {
+            msg = new List<string>();
+            lblPassword.Visible = false;
+            lblUsername.Visible = false;
+            lblname.Visible = false;
+            lbldesc.Visible = false;
+            lbldate.Visible = false;
+
+            bool result = true;
+            if (string.IsNullOrEmpty(txtname.Text))
+            {
+                msg.Add("The field (Name) is required");
+                lblname.Visible = true;
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                SaveForm();
+            }
+            else
+            {
+                string errors = string.Empty;
+                int errorIndex = 1;
+                foreach (var item in msg)
+                {
+                    errors += $"{errorIndex}. - {item.ToString()}\n";
+                    errorIndex++;
+                }
+                MessageBox.Show(errors, "Errors", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            GetClient();
+            DefaultControls();
+            btnadd.Enabled = false;
+            btnsave.Enabled = false;
+            btnsave.ForeColor = Color.Black;
+            btncancel.Enabled = false;
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            ClientId = String.Empty;
+            var test = dataGridView1.SelectedRows[0].Cells["Id"].Value.ToString();
+
+            if (!string.IsNullOrEmpty(dataGridView1.SelectedRows[0].Cells["Id"].Value.ToString()))
+            {
+                ClientId = dataGridView1.SelectedRows[0].Cells["Id"].Value.ToString();
+                btnupdate.Visible = true;
+                btndelete.Visible = true;
+            }
+            else
+            {
+                btndelete.Visible = false;
+                btnupdate.Visible = false;
+            }
+        }
+
+        private void GetClientById(string clientId)
+        {
+            DefaultControls();
+
+            var client = db.ClientTypes.FirstOrDefault(x => x.Id == Convert.ToInt32(clientId));
+
+            if (client != null)
+            {
+                txtname.Text = client.Name;
+                txtdesc.Text = client.Description;
+                txtdate.Text = client.CreatedDate.ToString();
+
+                var user = db.Users.FirstOrDefault(x => x.PeopleId == clientId);
+                if (user != null)
+                {
+                    txtUsername.Text = user.Username;
+                }
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            GetClientById(ClientId);
+            btnadd.Enabled = false;
+            btnsave.Enabled = true;
+            btncancel.Enabled = true;
+            btnupdate.Visible = false;
+            btndelete.Visible = false;
+        }
+    }
+}
